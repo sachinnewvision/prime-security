19c2f3257a51f070c840f13612b073979335d5f8
Sachinanilahire <iamworkingyet@gmail.com>
2024-06-24 06:56:38 +0530

    added main banner

--- src/components/homepage/Homepage.jsx
+++ src/components/homepage/Homepage.jsx
@@ -3,7 +3,16 @@ function Homepage() {
   return (
     <>
       <div className="parent-container">
-        <div className="left-container"></div>
+        <div className="left-container">
+          <div className="container">
+            <h1>Connecting You to Excellence</h1>
+            <h2>India’s most trusted manpower recruitment services</h2>
+            <div className="cta-buttons">
+              <button className="cta-button">Find Talent</button>
+              <button className="cta-button">Hire Us</button>
+            </div>
+          </div>
+        </div>
         <div className="right-container"></div>
       </div>
     </>
